885723b7a935049afed0292652136911dfe75c01
Lucas <lucasjbx@gmail.com>
2023-09-20 00:03:49 +0200

    volumen rotary funca!

--- code.py
+++ code.py
@@ -23,6 +23,7 @@ import neopixel
 import usb_hid
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
+import rotaryio
 
 ws_pin = board.GP22
 led_num = 16
@@ -138,96 +139,35 @@ BLACK = (0, 0, 0)
 
 ##############################rotary encoder##################################
 # Rotary encoder inputs with pullup on D3 & D4
-rot_a = DigitalInOut(board.GP19)
-rot_a.direction = Direction.INPUT
-rot_a.pull = Pull.UP
-rot_b = DigitalInOut(board.GP18)
-rot_b.direction = Direction.INPUT
-rot_b.pull = Pull.UP
-
-# the counter counts up and down, it can roll over! 16-bit value
-encoder_counter = 0
-# direction tells you the last tick which way it went
-encoder_direction = 0
-
-# constants to help us track what edge is what
-A_POSITION = 0
-B_POSITION = 1
-UNKNOWN_POSITION = -1  # initial state so we know if something went wrong
-
-rising_edge = falling_edge = UNKNOWN_POSITION
-
-# get initial/prev state and store at beginning
-#last_button = button.value
-rotary_prev_state = [rot_a.value, rot_b.value]
+# Rotary encoder
+enc = rotaryio.IncrementalEncoder(board.GP19, board.GP20)
+#encSw = digitalio.DigitalInOut(board.GP15)
+#encSw.direction = digitalio.Direction.INPUT
+#encSw.pull = digitalio.Pull.UP
+lastPosition = 0
+# USB device
+consumer = ConsumerControl(usb_hid.devices)
 ##############################################################################
 
 while True:
 #############################rotary encoder-volume############################
-    # reset encoder and wait for the next turn
-    encoder_direction = 0
-
-    # take a 'snapshot' of the rotary encoder state at this time
-    rotary_curr_state = [rot_a.value, rot_b.value]
-
-    if rotary_curr_state != rotary_prev_state:
-        #print("Changed")
-        if rotary_prev_state == [True, True]:
-            # we caught the first falling edge!
-            if not rotary_curr_state[A_POSITION]:
-                #print("Falling A")
-                falling_edge = A_POSITION
-            elif not rotary_curr_state[B_POSITION]:
-                #print("Falling B")
-                falling_edge = B_POSITION
-            else:
-                # uhh something went deeply wrong, lets start over
-                continue
-
-        if rotary_curr_state == [True, True]:
-            # Ok we hit the final rising edge
-            if not rotary_prev_state[B_POSITION]:
-                rising_edge = B_POSITION
-                print("Rising B")
-            elif not rotary_prev_state[A_POSITION]:
-                rising_edge = A_POSITION
-                print("Rising A")
-            else:
-                # uhh something went deeply wrong, lets start over
-                continue
-
-            # check first and last edge
-            if (rising_edge == A_POSITION) and (falling_edge == B_POSITION):
-                encoder_counter -= 1
-                encoder_direction = -1
-                print("%d dec" % encoder_counter)
-            elif (rising_edge == B_POSITION) and (falling_edge == A_POSITION):
-                encoder_counter += 1
-                encoder_direction = 1
-                print("%d inc" % encoder_counter)
-            else:
-                # (shrug) something didn't work out, oh well!
-                encoder_direction = 0
-
-            # reset our edge tracking
-            rising_edge = falling_edge = UNKNOWN_POSITION
-
-    rotary_prev_state = rotary_curr_state
-
-    # Check if rotary encoder went up
-    if encoder_direction == 1:
-        consumer.send(ConsumerControlCode.VOLUME_INCREMENT)
-        print("up")
-    # Check if rotary encoder went down
-    if encoder_direction == -1:
-        consumer.send(ConsumerControlCode.VOLUME_DECREMENT)
+    # poll encoder position
+    position = enc.position
+    if position != lastPosition:
+        led.value = True
+        if lastPosition < position:
+            consumer.send(ConsumerControlCode.VOLUME_INCREMENT)
+        else:
+            consumer.send(ConsumerControlCode.VOLUME_DECREMENT)
+        lastPosition = position
+        led.value = False
 
 
 ##############################################################################
     msg = usb_midi.receive()
     if msg != None:
         if str(msg)[0] != "M":
-            print(msg.value)
+            print(msg)
 ##light
             leds_to_light = round(msg.value/(127/led_num))
             if leds_to_light == 0:
